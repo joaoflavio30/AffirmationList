dbb33dfba6ffade6ecf5d0e913d24e9060169cf8
joaoflaviofreitas <joaoflaviofreitas@gmail.com>
2023-01-02 22:35:39 -0300

    Affirmation List with Card of Material Components and using recyclerview with adapter

---
+++ app/src/main/java/com/example/affirmationapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.affirmationapp
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.affirmationapp.adapter.ItemAdapter
+import com.example.affirmationapp.data.DataSource
+import com.example.affirmationapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myDataSet = DataSource().loadAffirmations()
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter = ItemAdapter(this , myDataSet)
+        recyclerView.setHasFixedSize(true)
+
+    }
+}
